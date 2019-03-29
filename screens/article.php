9d0785359d1dfe6cc3817290ac22ddb77e40e3dc
aguerram <agurram2013@gmail.com>
2019-03-29 21:31:56 +0300

    First commit

---
+++ screens/article.php
@@ -0,0 +1,131 @@
+<div>
+
+    <nav aria-label="breadcrumb col-12">
+        <ol class="breadcrumb col-12">
+            <li class="breadcrumb-item"><a href="<?= $env['link'] ?>">Accueil</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Article</li>
+        </ol>
+    </nav>
+    <div class="card col-12 p-0" style="border-radius:0;">
+        <div id="imagearticle">
+
+        </div>
+        <div class="card-body col-12 " style="border-radius:0">
+            <h5 class="card-title" id="title"></h5>
+            <p class="card-text text-justify" id="content"></p>
+            <p class="row justify-content-around clearnotactivated">
+                <small><i class="fa fa-user"></i> Par : <a href="<?= route('profile/'.$params['1']) ?>"><span id="by"></span> </a> </small>
+                <small><i class="fa fa-calendar"></i> Publié à : <span id="at"></span></small>
+            </p>
+            <div id="commentsection" class="m-1 m-md-2 m-lg-4 shadow-sm clearnotactivated">
+                <?php
+                if (!$loggedin) {
+                    ?>
+                    <form class="p-0 p-md-2">
+                        <div class="form-group row">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Nom complete</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" placeholder="Nom complete">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Commentaire</label>
+                            <div class="col-sm-10">
+                                <textarea type="text" class="form-control" placeholder="Commentaire"></textarea>
+                            </div>
+                        </div>
+                    </form>
+                    <?php
+                } else if(false) {
+                    ?>
+                    <form class="p-0 p-md-2" method="post" action="<?= api("addCommentUser") ?>">
+                        <div class="form-group row">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Commentaire</label>
+                            <div class="col-sm-10">
+                                <textarea required name="comment" type="text" class="form-control"
+                                          placeholder="Commentaire"></textarea>
+                            </div>
+                        </div>
+                        <input type="hidden" id="arid" name="arid" value="">
+                        <div class="row justify-content-end">
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-send" aria-hidden="true"></i>
+                                Envoyer
+                            </button>
+                        </div>
+                    </form>
+
+                    <?php
+                }
+                ?>
+
+                <hr>
+                <div>
+                    <div class="list-group" id="comments"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $.ajax({
+        type: "post",
+        dataType: "json",
+        url: "<?= $env["link"] ?>api/getArticleById.php",
+        data: {"id": <?= $params[1] ?>},
+        success: function (data) {
+            if (data.success) {
+                var da = data.data;
+                if(da.ACTIVE == 1 || data.editor)
+                {
+                    $("#title").text(da['TITRE_ARTICLE']);
+                    $("#at").text(da['DATE_PUB']);
+                    $("#content").text(da['CONTENU_ARTICLE']);
+                    $("#by").text(data.by);
+                    $("#arid").val(da['ID_ARTICLE']);
+                    $("#imagearticle").html('<img src="<?= $env['storage'] ?>' + da['IMG_ARTICLE'] + '" class="card-img-top" style="border-radius:0" alt="...">');
+
+
+                    //Comments
+                    data.comments.forEach(function(e){
+                        var byuser = e['full_name'];
+                        var attime = e['at'];
+                        var contentcom = e['content'];
+                        var html = '<div href="#" class="list-group-item list-group-item-action mt-1"><div class="d-flex w-100 justify-content-between "><h5 class="mb-1">'+byuser+'</h5><small><i class="fa fa-clock-o"></i> '+attime+'</small></div><p class="mb-1 ml-4">'+contentcom+'</p></div>';
+                        $("#comments").append(html);
+                    });
+                }
+                else{
+                    $("#content").html("<div class='alert alert-danger'><i class='fa fa-stop-circle'></i> Le contenu de cette page ne peut pas être affiché pour certaines raisons</div>");
+                    $(".clearnotactivated").html("");
+                }
+
+            }
+        }
+    })
+    $("form").ajaxForm({
+        success: function (data) {
+            if (data.success) {
+                swal({
+                    icon:"success",
+                    text:"Votre commentaire a été ajouté avec success"
+                })
+                $("form").trigger("reset");
+                setTimeout(function () {
+                    location.reload()
+                },1000);
+            }
+            else{
+                swal({
+                    icon:"success",
+                    text:"Une erreur inattendue s'est produite"
+                })
+            }
+        }
+    })
+</script>
